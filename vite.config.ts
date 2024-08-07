0706ebaab1234faef4e5c75fac261f08ce6f044c
mintsweet <0x1304570@gmail.com>
2024-08-07 19:59:33 +1200

    feat: initialize library hooks

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import { dependencies, peerDependencies } from './package.json';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'hooks',
+      fileName: 'hooks',
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(dependencies),
+        ...Object.keys(peerDependencies),
+      ],
+    },
+  },
+});
