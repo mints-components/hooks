c5786e4b36606a2393254252bdd8e25fb2e4cf59
mintsweet <0x1304570@gmail.com>
2024-09-06 21:03:04 +1200

    feat: output type definition file

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { resolve } from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 import { dependencies, peerDependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+      outDir: 'types',
+      rollupTypes: true,
+    }),
+  ],
 
   build: {
     lib: {
