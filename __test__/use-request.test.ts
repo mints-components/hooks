bac1c8d0af7247a9113df1d2791fa6631bbed84b
mintsweet <0x1304570@gmail.com>
2024-09-11 11:12:52 +1200

    feat: optimizing the useRequest hook

--- __test__/use-request.test.ts
+++ __test__/use-request.test.ts
@@ -4,84 +4,68 @@ import { useRequest } from '../src';
 
 const request = jest.fn();
 
+jest.useFakeTimers();
+
 describe('useRequest', () => {
   it('should return the initial values for data, loading, error', async () => {
     const { result } = renderHook(() => useRequest(request));
 
-    const { loading, data, error } = result.current;
-
-    expect(loading).toBe(true);
-    expect(data).toBe(undefined);
-    expect(error).toBe(undefined);
-  });
-});
-
-describe('when data is fetched successfully', () => {
-  let mockedData;
-
-  beforeEach(() => {
-    mockedData = [
-      {
-        id: 1,
-        title: 'mock title',
-      },
-    ];
-
-    request.mockResolvedValue(mockedData);
+    expect(result.current).toEqual({
+      loading: false,
+    });
   });
 
-  it('should return data', async () => {
+  it('should return the loading state when the request is pending', async () => {
+    request.mockImplementation(
+      () => new Promise((resolve) => setTimeout(() => resolve('data'), 1000)),
+    );
+
+    const { result, rerender } = renderHook(() => useRequest(request));
+
+    expect(result.current).toEqual({
+      loading: false,
+    });
+
+    jest.advanceTimersByTime(500);
+    rerender();
+    expect(result.current).toEqual({
+      loading: true,
+    });
+  });
+
+  it('should return the data after the request is resolved', async () => {
+    request.mockResolvedValueOnce('data');
+
     const { result } = renderHook(() => useRequest(request));
 
     await waitFor(() =>
       expect(result.current).toEqual({
         loading: false,
-        data: mockedData,
+        data: 'data',
       }),
     );
   });
-});
 
-describe('the loading property', () => {
-  it('should initially return true and then false', async () => {
-    const { result } = renderHook(() => useRequest(request));
-    const { loading } = result.current;
+  it('should return the error state when the request is rejected', async () => {
+    const error = new Error('mocked error');
+    request.mockRejectedValueOnce(error);
 
-    // asserting that the initial value of loading is true
-    // before the re-render
-    expect(loading).toBe(true);
-
-    await waitFor(() => {
-      const { loading } = result.current;
-      expect(loading).toBe(false);
-    });
-  });
-});
-
-describe('when data is not fetched successfully', () => {
-  const mockedError = new Error('mocked error');
-
-  beforeEach(() => {
-    // we mock fetch to return a rejected value so that
-    // we add some coverage in the catch block in our code
-    request.mockRejectedValue(mockedError);
-  });
-
-  it('should return the Error', async () => {
     const { result } = renderHook(() => useRequest(request));
 
-    await waitFor(() => {
-      const { error } = result.current;
-      expect(error).toBe(mockedError);
-    });
+    await waitFor(() =>
+      expect(result.current).toEqual({
+        loading: false,
+        error,
+      }),
+    );
   });
-});
 
-describe('should abort the fetch request on unmount', () => {
-  const mockAbort = jest.spyOn(AbortController.prototype, 'abort');
+  it('should abort the fetch request on unmount', () => {
+    const mockAbort = jest.spyOn(AbortController.prototype, 'abort');
 
-  it('should abort the fetch request', async () => {
     const { unmount } = renderHook(() => useRequest(request));
+
+    jest.advanceTimersByTime(10);
     unmount();
 
     expect(mockAbort).toHaveBeenCalled();
--- src/use-request.ts
+++ src/use-request.ts
@@ -8,9 +8,9 @@ export const useRequest = <T>(
   const [, setVersion] = useState(0);
   const ref = useRef<{
     state: 'idle' | 'loading' | 'done' | 'error';
-    deps?: React.DependencyList;
     data?: T;
     error?: unknown;
+    deps?: React.DependencyList;
     abortController?: AbortController;
     timer?: number;
   }>({
@@ -32,30 +32,30 @@ export const useRequest = <T>(
     };
   }
 
-  // When the last state transition has not waited until the new request is completed
-  // Reset status to pending
-  ref.current.state = 'loading';
-  ref.current.deps = deps;
-  ref.current.data = undefined;
-
   clearTimeout(ref.current.timer);
-
+  ref.current.deps = deps;
   ref.current.abortController?.abort();
+
   ref.current.timer = window.setTimeout(() => {
+    ref.current.state = 'loading';
     ref.current.abortController = new AbortController();
     request(ref.current.abortController.signal)
       .then((data: T) => {
         ref.current.data = data;
         ref.current.state = 'done';
-        setVersion((v) => v + 1);
       })
       .catch((err: unknown) => {
         ref.current.error = err;
+        ref.current.state = 'error';
+      })
+      .finally(() => {
         setVersion((v) => v + 1);
       });
   }, 10);
 
   return {
-    loading: true,
+    loading: ref.current.state === 'loading',
+    data: ref.current.data,
+    error: ref.current.error,
   };
 };
