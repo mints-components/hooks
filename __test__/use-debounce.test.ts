295e2fc181b0cb0f8b6f0d11ecae764a42e7edf3
mintsweet <0x1304570@gmail.com>
2024-09-11 12:16:28 +1200

    refactor: the unit test of useDebounce

--- __test__/use-debounce.test.ts
+++ __test__/use-debounce.test.ts
@@ -5,33 +5,38 @@ import { useDebounce } from '../src';
 jest.useFakeTimers();
 
 describe('useDebounce', () => {
-  it('useDebounce wait:200ms', async () => {
-    let mountedState = 0;
-    const { result, rerender } = renderHook(() =>
-      useDebounce(mountedState, { wait: 200 }),
-    );
+  let state = 0;
+
+  afterEach(() => {
+    state = 0;
+  });
+
+  it('should not change when the time has not arrived', async () => {
+    const { result, rerender } = renderHook(() => useDebounce(state));
     expect(result.current).toBe(0);
 
-    mountedState = 1;
+    state = 1;
     rerender();
-    jest.advanceTimersByTime(50);
     expect(result.current).toBe(0);
 
-    mountedState = 2;
+    jest.advanceTimersByTime(500);
+
+    state = 2;
     rerender();
-    jest.advanceTimersByTime(100);
+    expect(result.current).toBe(0);
+  });
+
+  it('should change when the time has arrived', async () => {
+    const { result, rerender } = renderHook(() => useDebounce(state));
     expect(result.current).toBe(0);
 
-    mountedState = 3;
+    state = 1;
     rerender();
-    jest.advanceTimersByTime(150);
     expect(result.current).toBe(0);
 
-    mountedState = 4;
-    rerender();
     await act(async () => {
-      jest.advanceTimersByTime(200);
+      jest.advanceTimersByTime(1000);
     });
-    expect(result.current).toBe(4);
+    expect(result.current).toBe(1);
   });
 });
