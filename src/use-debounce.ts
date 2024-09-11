bf2de57b4e8b001242ef3963f608a024376b9132
mintsweet <0x1304570@gmail.com>
2024-09-11 12:15:59 +1200

    fix: useDebounce options no need to fill

--- src/use-debounce.ts
+++ src/use-debounce.ts
@@ -3,14 +3,21 @@ import { debounce } from 'lodash';
 
 export const useDebounce = <T>(
   value: T,
-  {
+  options?: {
+    wait?: number;
+    leading?: boolean;
+    maxWait?: number;
+    trailing?: boolean;
+  },
+): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  const {
     wait = 1000,
     leading = false,
     maxWait,
     trailing = true,
-  }: { wait?: number; leading?: boolean; maxWait?: number; trailing?: boolean },
-): T => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
+  } = options || {};
 
   useEffect(() => {
     const debounced = debounce(() => setDebouncedValue(value), wait, {
