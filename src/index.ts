ef30ec51e12f1f92f7ce1d5dc268225b59e2d6b5
mintsweet <0x1304570@gmail.com>
2025-08-18 11:43:07 +1200

    fix: not export useTimer

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './use-debounce';
 export * from './use-hotkeys';
 export * from './use-outside-click';
 export * from './use-storage';
+export * from './use-timer';
 export * from './use-toggle';
 export * from './use-url-state';
