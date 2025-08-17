3fdf28e80661e85ede75f5e9e06cc17519f3c8b4
mintsweet <0x1304570@gmail.com>
2025-08-18 09:59:10 +1200

    refactor: remove hook useQueryParams

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from './use-debounce';
 export * from './use-hot-keys';
 export * from './use-localstorage';
 export * from './use-outside-click';
-export * from './use-query-params';
 export * from './use-request';
 export * from './use-toggle';
 export * from './use-url-state';
